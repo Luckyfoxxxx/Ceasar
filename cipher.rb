a6fdbffa67fcf3304b068d853d96981be379d77b
Mikael <secretunicornclub@gmail.com>
2016-09-13 14:02:24 +0200

    First Ceasar commit

---
+++ cipher.rb
@@ -0,0 +1,35 @@
+#Ceasar cipher
+
+def ceasar (sentence, integer)
+	unaltered = sentence.split""
+	
+	
+
+	integer.times do
+		unaltered.each do |c|
+			c.next!
+			if c == "aa"
+				 c.chop!
+			end
+
+		end
+	end 
+	unaltered = unaltered.join("")
+	puts "#{sentence} ble til #{unaltered} "
+end
+
+
+puts "Vennligst skriv inn en setning og shift integer"
+ukryptert = gets.chomp
+velg_tall = gets.chomp.to_i
+
+ceasar(ukryptert, velg_tall)
+
+
+
+ 
+
+
+
+ 
+
